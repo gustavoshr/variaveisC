10ea7b0d32312e6f935bc6e55c13b3d6a2b78173
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:14:52 -0300

    variaveisc

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TIPO NOME = VALOR
+            int idade = 43;
+            idade = 34;
+
+            const int ano = 2021;
+            ano = 2021;
+            Console.WriteLine(ano);
+
+            // int, double, chat, boolean, string Esses são os dados mais usados
+            
+        }
+    }
+}
